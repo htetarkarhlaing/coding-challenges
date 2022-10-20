71d82442d0486f75779fb6de7497d046ed1a8331
Htet Arkar Hlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-10-20 13:23:50 +0630

    Create format.js
    Added: Array formatting code sample.

---
+++ format.js
@@ -0,0 +1,72 @@
+let payload = {
+	"Sagaing Region": [
+		{
+			level_3: "Yinmabin",
+		},
+		{
+			level_3: "Tamu",
+		},
+	],
+	Kachin: [
+		{
+			level_3: "Chipwi",
+		},
+		{
+			level_3: "Shwegu",
+		},
+	],
+};
+
+let objArr = Object.keys(payload);
+console.log(objArr);
+let tempArr = [];
+for (let index = 0; index < objArr.length; index++) {
+	const element = objArr[index];
+	let subTempData = {
+		label: element,
+		options: [],
+	};
+	for (let subIndex = 0; subIndex < payload[element].length; subIndex++) {
+		const subElem = payload[element][subIndex];
+		if (subIndex === 0) {
+			subTempData.options.push(
+				{
+					value: element,
+					label: element,
+				},
+				{
+					value: subElem.level_3,
+					label: subElem.level_3,
+				}
+			);
+		} else {
+			subTempData.options.push({
+				value: subElem.level_3,
+				label: subElem.level_3,
+			});
+		}
+	}
+	tempArr.push(subTempData);
+}
+
+let result = [
+	{
+		label: "Sagaing Region",
+		options: [
+			{ value: "Sagaing Region", label: "Sagaing Region" },
+			{ value: "Yinmabin", label: "Yinmabin" },
+			{ value: "Tamu", label: "Tamu" },
+		],
+	},
+	{
+		label: "Kachin",
+		options: [
+			{ value: "Kachin", label: "Kachin" },
+			{ value: "Chipwi", label: "Chipwi" },
+			{ value: "Shwegu", label: "Shwegu" },
+		],
+	},
+];
+
+console.log(tempArr);
+console.log(result);
